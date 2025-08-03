3ad916e521d1f082c150dc9309db97cf49092793
Chris F <cmfredricksen@gmail.com>
2025-08-03 12:26:37 -0500

    update css icon, colors, content.config

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,15 +2,15 @@ import { defineCollection, z } from "astro:content";
 import { glob, file } from "astro/loaders"
 
 const blog = defineCollection({
-    loader: glob({pattern: "**/*.md", base: "./pages/posts"}),
+    loader: glob({pattern: "**/*.mdx", base: "./src/pages/posts"}),
     schema: z.object({
         title: z.string(),
         author: z.string(),
-        date: z.date(),
+        date: z.date().optional(),
         description: z.string(),
         path: z.string(),
         layout: z.string(),
-        tags: z.array(z.string())
+        tags: z.array(z.string()).optional()
     })
 })
 
